b06c9b9cccb83d9e2c5fa9ad8f9b6a2893c10c5c
Claudiu Diaconeasa <diaconeasaclaudiu@gmail.com>
2020-03-08 15:02:00 +0000

    Generated csv based on the form is passed to the script with the right format now Ready to get downloaded Issue using the randomiser in .py for now

--- src/main/java/corpus/EventSelector.java
+++ src/main/java/corpus/EventSelector.java
@@ -94,7 +94,8 @@ public class EventSelector extends HttpServlet {
         //List of arrays having each column of a row, basically all the entries corresponding to each column just on that particular row
         ArrayList<String[]> selectedRecords = new ArrayList<String[]>();
 
-        for (int i = 0; i < selection.size(); i++) {
+        for (int i = 0; i < selection.size(); i++)
+        {
             Integer rowNumber = Integer.parseInt(selection.get(i));
             selectedRecords.add(records.get(rowNumber));
         }
@@ -104,201 +105,70 @@ public class EventSelector extends HttpServlet {
         FileWriter outputfile = new FileWriter(file);
         CSVWriter csvWriter = new CSVWriter(outputfile);
 
-        String[] columnNames = {"Title", "Start", "Description", "Place", "Location", "Media", "Source URL"};
-        csvWriter.writeNext(columnNames);
+        String[] columnNames = {"Title", "Date", "Description", "Place", "Location", "Media", "Source URL"};
+        //Writes the first row without quotations (header) to match the .py script format
+        csvWriter.writeNext(columnNames,false);
 
+        //Writes all records using quotations
         csvWriter.writeAll(selectedRecords);
+
         csvWriter.close();
 
-        //A python script for generating the calendar as a .pdf will be run
-        String pythonOutput = null;
-
-//        String scriptPath = "python /Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/java/corpus/CalendarGenerator/callirhoe/callirhoe.py /Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/java/corpus/CalendarGenerator/callirhoe/Events/test.csv";
-//        String pythonpath = "/usr/bin/python2.7"
-//        File pdfFile = doPostRequestToPythonServer(csv);
-//        Process p = Runtime.getRuntime().exec( "python /Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/java/corpus/CalendarGenerator/callirhoe/creator.py /Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/java/corpus/CalendarGenerator/callirhoe/Events/test.csv");
-//        if (p.isAlive())
-//        {
-//            Process pTwo = Runtime.getRuntime().exec("python calmagick '/Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/java/corpus/CalendarGenerator/callirhoe/Resources/background.jpg' --range=1:12/2018 --outdir=expore -j 2 --with-holidays=/Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/java/corpus/CalendarGenerator/callirhoe/holidays");
-//
-//        }
-//        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-//        //The command will have an output, it will be written here
-//        System.out.println("Output of the python script:\n");
-//        while ((pythonOutput = stdInput.readLine()) != null) {
-//            System.out.println(pythonOutput);
-//        }
-
-//        System.out.println(System.getProperty("user.dir"));
-
-        //Generates a pdf from the form
-//        sendPythonRequest();
-
-        sendCsvRequest("http://127.0.0.1:5000/generate",  "auth",  "/Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/web/template/generatedCsv/test.csv",  "success", request, response);
-//        File pdfFile = doPostRequestToPythonServer(csv);
-
-//        generatePdf(request, response);
-
-
+        sendCsvRequest("http://127.0.0.1:5000/generate","/Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/web/template/generatedCsv/historyGenerated.csv",
+                       "success", request, response);
     }
 
 
-    public void sendCsvRequest(String url, String authData, String attachmentFilePath, String outputFilePathName, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String charset = "UTF-8";
-        File binaryFile = new File(attachmentFilePath);
-        String boundary = "------------------------" + Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
-        String CRLF = "\r\n"; // Line separator required by multipart/form-data.
-        int responseCode = 0;
+    public void sendCsvRequest(String url, String attachmentFilePath, String outputFilePathName, HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        File file = new File(attachmentFilePath);
 
-        try {
             //Set POST general headers along with the boundary string (the seperator string of each part)
             URLConnection connection = new URL(url).openConnection();
             connection.setDoOutput(true);
+
+             String boundary = "------------------------" + Long.toHexString(System.currentTimeMillis());
             connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-            connection.addRequestProperty("User-Agent", "CheckpaySrv/1.0.0");
-            connection.addRequestProperty("Accept", "*/*");
-            connection.addRequestProperty("Authentication", authData);
 
             OutputStream output = connection.getOutputStream();
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
 
-            // Send binary file - part
-            // Part header
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, "UTF-8"), true);
+
+            String CRLF = "\r\n"; // Line separator required by multipart/form-data.
             writer.append("--" + boundary).append(CRLF);
-            writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + binaryFile.getName() + "\"").append(CRLF);
+            writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"").append(CRLF);
             writer.append("Content-Type: application/octet-stream").append(CRLF);// + URLConnection.guessContentTypeFromName(binaryFile.getName())).append(CRLF);
             writer.append(CRLF).flush();
 
             // File data
-            Files.copy(binaryFile.toPath(), output);
+            Files.copy(file.toPath(), output);
             output.flush();
 
             // End of multipart/form-data.
             writer.append(CRLF).append("--" + boundary + "--").flush();
 
-            responseCode = ((HttpURLConnection) connection).getResponseCode();
-
-            if (responseCode != 200) //We operate only on HTTP code 200
-                return;
             InputStream Instream = ((HttpURLConnection) connection).getInputStream();
 
             // Write csv file
-            BufferedInputStream BISin = new BufferedInputStream(Instream);
-            FileOutputStream FOSfile = new FileOutputStream(outputFilePathName);
-            BufferedOutputStream out = new BufferedOutputStream(FOSfile);
+            BufferedInputStream bufferInput = new BufferedInputStream(Instream);
 
+            //Readin from this path
+            FileOutputStream fileOut = new FileOutputStream(outputFilePathName);
+
+            //Stream to write byte by byte
+            BufferedOutputStream finalOut = new BufferedOutputStream(fileOut);
+
+            response.setContentType("application/octet-stream");
             int i;
-            while ((i = BISin.read()) != -1) {
-                out.write(i);
+            while ((i = bufferInput.read()) != -1)
+            {
+                finalOut.write(i);
+                //Downloading the file
+                response.getOutputStream().write(i);
             }
 
-            ((HttpURLConnection) connection).setRequestMethod("POST");
-
-//            // Writing the post data to the HTTP request body
-//            BufferedWriter httpRequestBodyWriter = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-//            httpRequestBodyWriter.write("visitorName=Johnny+Jacobs&luckyNumber=1234");
-//            httpRequestBodyWriter.close();
-
-            // Get a readable channel from url connection
-            ReadableByteChannel readableChannelForHttpResponseBody = Channels.newChannel(connection.getInputStream());
-
-            // Create the file channel to save file
-            FileOutputStream fosForDownloadedFile = new FileOutputStream("calendar.pdf");
-            FileChannel fileChannelForDownloadedFile = fosForDownloadedFile.getChannel();
-
-            // Save the contents of HTTP response to local file
-            fileChannelForDownloadedFile.transferFrom(readableChannelForHttpResponseBody, 0, Long.MAX_VALUE);
-
-//
-
-
-
-            // Cleanup
-            out.flush();
-            out.close();
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-//        File downloadFile = new File("/Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/web/template/generatedCsv/test.csv");
-////        String relativePath = downloadFile.getCanonicalPath();
-//
-////        System.out.println(relativePath);
-//
-////        File passDownloadFile = new File(relativePath);
-//        FileInputStream in = new FileInputStream(downloadFile);
-//
-//        response.setContentType("application/octet-stream");
-//        response.setContentLength((int) downloadFile.length());
-//
-//        // Set file to be returned as the pdf to the user - download
-//        String key = "Content-Disposition";
-//        String value = String.format("attachment; filename=\"%s\"", downloadFile.getName());
-//
-//        response.setHeader(key, value);
-//
-//        //Returning the stream from the response - actually receiving the file
-//        OutputStream outFile = response.getOutputStream();
-//
-//        byte[] buffer = new byte[4096];
-//        int bytes = -1;
-//
-//        while ((bytes = in.read(buffer)) != -1)
-//        {
-//            outFile.write(buffer, 0, bytes);
-//        }
-
-
-
     }
 
-    private void generatePdf(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException
-    {
-
-//       String path = "/../../template/pdfFiles/FirstCalendar (1).pdf";
-//       String relative =
-//       String absolutePath = path.toRelativePath
-//        String path = getClass().getResource("FirstCalendar (1).pdf").getPath();
-//        template/pdfFiles/FirstCalendar (1).pdf
-//        String path = "template/pdfFiles/FirstCalendar (1).pdf";
-//        String base = "/template";
-//
-//        String relative = new File(base).toURI().relativize(new File(path).toURI()).getPath();
-
-
-        File downloadFile = new File("/Users/claudiudiaconeasa/Documents/Claudiu/thirdyearproject/src/main/web/template/pdfFiles/FirstCalendar (1).pdf");
-        String relativePath = downloadFile.getCanonicalPath();
-
-        System.out.println(relativePath);
-
-        File passDownloadFile = new File(relativePath);
-        FileInputStream in = new FileInputStream(passDownloadFile);
-
-        response.setContentType("application/octet-stream");
-        response.setContentLength((int) downloadFile.length());
-
-        // Set file to be returned as the pdf to the user - download
-        String key = "Content-Disposition";
-        String value = String.format("attachment; filename=\"%s\"", downloadFile.getName());
-
-        response.setHeader(key, value);
-
-        //Returning the stream from the response - actually receiving the file
-        OutputStream out = response.getOutputStream();
-
-        byte[] buffer = new byte[4096];
-        int bytes = -1;
-
-        while ((bytes = in.read(buffer)) != -1)
-        {
-            out.write(buffer, 0, bytes);
-        }
-
-
-    }
 }
 
 
